d5cf7fff5997c5871ab0321230806e7c6fd8be66
Joe Beeson <jbeeson@gmail.com>
2010-05-10 13:29:06 -0600

    Initial commit

---
+++ views/helpers/analogue.php
@@ -0,0 +1,78 @@
+<?php
+
+	/**
+	 * AnalogueHelper
+	 * Renames, or "maps", helpers prior to rendering so that you can easily
+	 * replace helpers with your extended, or updated version.
+	 * @author Joe Beeson <jbeeson@gmail.com>
+	 */
+	class AnalogueHelper extends AppHelper {
+
+		/**
+		 * Mappings
+		 * @var array
+		 * @access private
+		 */
+		private $mappings = array();
+		
+		/**
+		 * View
+		 * @var View
+		 * @access private
+		 */
+		private $view;
+		
+		/**
+		 * Executed prior to rendering
+		 * @return null
+		 * @access public
+		 */
+		public function beforeRender() {
+			
+			// Loop through and call _mapHelper()
+			foreach ($this->mappings as $mapping) {
+				$this->_mapHelper($mapping);
+			}
+		}
+		
+		/**
+		 * Construction
+		 * @param array $settings
+		 * @return null
+		 * @access public
+		 */
+		public function __construct($mappings = array()) {
+			
+			// Grab our View object for use later...
+			$this->view = ClassRegistry::getObject('view');
+			
+			// Merge our mappings together...
+			$this->mappings = am(
+				$this->mappings,
+				$mappings
+			);
+		}
+		
+		/**
+		 * Performs the mapping of a helper object to a new name
+		 * @param array $mapping
+		 * @return null
+		 * @access private
+		 */
+		private function _mapHelper($mapping = array()) {
+			
+			// Helpers are always lowercased in the View object
+			$mapping = array_map('strtolower', $mapping);
+			
+			// Extract our array for use
+			extract($mapping);
+			
+			// Only continue if we have a valid, loaded helper
+			if (isset($helper)) {
+				if (isset($this->view->loaded[$helper]) and isset($rename)) {
+					$this->view->loaded[$rename] = $this->view->loaded[$helper];
+				}
+			}
+		}
+		
+	}
